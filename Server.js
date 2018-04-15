6c41450aa06c9f5dd3be89c7bf9cb14b63e31322
soojkim0306 <31287593+soojkim0306@users.noreply.github.com>
2018-04-14 17:52:16 -0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -71,7 +71,7 @@
             //response.writeHead(响应状态码，响应头对象): 发送一个响应头给请求。
             response.writeHead(200,{'Content-Type':'text/html'})
             // 如果url=‘/’ ,读取指定文件下的html文件，渲染到页面。
-            fs.readFile('./googleMap.html','utf-8',function(err,data){
+            fs.readFile('./home.html','utf-8',function(err,data){
                 if(err){
                     throw err ;
                 }
