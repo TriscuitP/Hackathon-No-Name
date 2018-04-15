020ce13be73979570b7f6045401b9982a068996a
dknoma <dknoma@dons.usfca.edu>
2018-04-14 20:53:59 -0700

    changed user/pw functionality
    added a pw check for registration and loging in. will create messages depending on whether or not the user was successful

--- Data.js
+++ Data.js
@@ -6,8 +6,8 @@
 
 // var user = {items: items, amount: amount, prices: prices};
 
-var express = require('express');
-var app = exress();
+// var express = require('express');
+// var app = exress();
 
 function addUser() {
 	var name = document.getElementById("name").value;
@@ -24,17 +24,16 @@ function addUser() {
 		document.getElementById("exists").innerHTML = "Username " + obj.username + 
 		" already exists! Please choose another username.";
 	} else {
-		// if(pw === pwCheck) {
-			var userObj = { "username":name, 
-								"password":pw };
+		//passwords should not include \"
+		if(pw === pwCheck && !pw.includes("\"")) {
+			var userObj = { "username":name, "password":pw };
 			var userJSON = JSON.stringify(userObj);
 			localStorage.setItem(name, userJSON);
 
 			document.getElementById("exists").innerHTML = "Successfully created account!";
-		// } else {
-		// 	document.getElementById("exists").innerHTML = "Passwords do not match. Please try again";
-		// }
-		// document.write(true);
+		} else {
+			document.getElementById("exists").innerHTML = "Passwords do not match or is invalid. Please try again";
+		}
 	}
 }
 
@@ -43,12 +42,20 @@ function login() {
 	var pw = document.getElementById("password").value;
 
 	var text = localStorage.getItem(name);
-	var obj = JSON.parse(text);
+	//if there was no json to parse, then username was not found
+	if(!JSON.parse(text)){
+		document.getElementById("exists").innerHTML 
+		= "User not found. Please try again. If you do not have an account, please create one";
+	} else {
+		var obj = JSON.parse(text);
+		//gets pw with quotations, this will do a crude removal of \" as passwords will not contain \"
+		var pass = obj.password.split("\"")[1];
 
-	// document.getElementById("exists").innerHTML = obj.password;
-
-	// if(obj.password == pw) {
-	// 	document.getElementById("exists").innerHTML = "Login successful!";
-	// }
+		if(pw == pass) {
+			document.getElementById("exists").innerHTML = "Login successful!";
+		} else {
+			document.getElementById("exists").innerHTML = "Password is invalid. Please try again.";
+		}
+	}
 }
 
